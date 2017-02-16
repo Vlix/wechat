0130ae57414411e7e165506a306dd8c0789a1270
Joeri van Eekelen <joeri@botboys.com>
2017-02-16 18:21:57 +0100

    We print XML now!

--- Web/WeChat/XMLPrint.hs
+++ Web/WeChat/XMLPrint.hs
@@ -2,6 +2,7 @@ module Web.WeChat.XMLPrint where
 
 import           Data.Maybe (maybeToList)
 import qualified Data.Text as T
+import           Data.Time (UTCTime)
 import           Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
 
 import           Text.XML.Light
@@ -28,9 +29,7 @@ showTag :: Show a => T.Text -> a -> Element
 showTag lbl thing = stringTag lbl (show thing)
 
 printOutMessage :: OutCallbackMessage -> T.Text
-printOutMessage = T.pack . showElement . xmlEnvelope . printOutMessage'
-  where xmlEnvelope :: [Element] -> Element
-        xmlEnvelope elts = Element (tag "xml") [] (map Elem elts) Nothing
+printOutMessage = T.pack . showElement . eltTag "xml" . printOutMessage'
 
 printOutMessage' :: OutCallbackMessage -> [Element]
 printOutMessage' (OutMessage from to ct cont) =
@@ -41,10 +40,10 @@ printOutMessage' (OutMessage from to ct cont) =
   where dispTime :: UTCTime -> Integer
         dispTime = floor . utcTimeToPOSIXSeconds
 
-outXML :: Text -> [Element] -> [Element]
+outXML :: T.Text -> [Element] -> [Element]
 outXML typ elts = textTag "MsgType" typ : elts
 
-mediaXML :: Text -> [Element] -> [Element]
+mediaXML :: T.Text -> [Element] -> [Element]
 mediaXML typ elts = outXML (T.toLower typ) [eltTag typ elts]
 
 mbTag :: (a -> b) -> Maybe a -> [b]
@@ -57,22 +56,20 @@ printOutContent (OutAudio mID) = mediaXML "Voice" [textTag "MediaId" mID]
 printOutContent (OutVideo mId thMId) = mediaXML "Video"
   [textTag "MediaId" mId, textTag "ThumbMediaId" thMId]
 printOutContent (OutMusic mbTitle mbDescr mbURL mbHQURL mID) =
-  outXML "music" $ eltTag "Music" $
+  mediaXML "Music" $
     mbTag (textTag "Title") mbTitle ++
     mbTag (textTag "Description") mbDescr ++
     mbTag (textTag "MusicUrl") mbURL ++
     mbTag (textTag "HQMusicUrl") mbHQURL ++
     [textTag "ThumbMediaId" mID]
+printOutContent (OutRich articles) = outXML "news" $
+  [showTag "ArticleCount" (length articles)] ++
+  map printOutArticle articles
 
-
-
-
-
-
-
-
-
-
-
-
+printOutArticle :: OutRichArticle -> Element
+printOutArticle (OutRichArticle mbTitle mbDescr mbPicURL mbURL) = eltTag "item" $
+    mbTag (textTag "Title") mbTitle ++
+    mbTag (textTag "Description") mbDescr ++
+    mbTag (textTag "PicUrl") mbPicURL ++
+    mbTag (textTag "Url") mbURL
 
